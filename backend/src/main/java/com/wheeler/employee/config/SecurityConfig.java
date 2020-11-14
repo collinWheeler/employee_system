699618ba99104e936c2a184790f74cb264eac144
collinWheeler <collinWheeler@users.noreply.github.com>
2020-11-13 22:08:42 -0500

    Added login via username/password.

---
+++ backend/src/main/java/com/wheeler/employee/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.wheeler.employee.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final String secureUserName;
+	private final String secureUserPassWord;
+	
+	public SecurityConfig(@Value("${secure.user.username}") String secureUserName, @Value("${secure.user.password}") String secureUserPassWord){
+		this.secureUserName = secureUserName;
+		this.secureUserPassWord = secureUserPassWord;
+	}
+	
+	@Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authentication)
+            throws Exception
+    {
+        authentication.inMemoryAuthentication()
+                .withUser(secureUserName)
+                .password(passwordEncoder().encode(secureUserPassWord))
+                .authorities("ROLE_USER");
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
